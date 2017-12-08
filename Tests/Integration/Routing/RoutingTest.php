5a21241cbfd1aba9b1d2ca82d68d9de28bdca737
Oliver Klee <oliver@phplist.com>
2017-12-08 16:39:18 +0100

    [CLEANUP] Move the tests for the root URL into a separate test case

---
+++ Tests/Integration/Routing/RoutingTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace PhpList\RestBundle\Tests\Integration\Routing;
+
+use PhpList\PhpList4\Core\Bootstrap;
+use PhpList\PhpList4\Core\Environment;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Testcase.
+ *
+ * @author Oliver Klee <oliver@phplist.com>
+ */
+class RoutingTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    private $client = null;
+
+    protected function setUp()
+    {
+        Bootstrap::getInstance()->setEnvironment(Environment::TESTING)->configure();
+
+        $this->client = self::createClient(['environment' => Environment::TESTING]);
+    }
+
+    protected function tearDown()
+    {
+        Bootstrap::purgeInstance();
+    }
+
+    /**
+     * @test
+     */
+    public function rootUrlHasHtmlContentType()
+    {
+        $this->client->request('get', '/');
+
+        $response = $this->client->getResponse();
+
+        self::assertContains('text/html', (string)$response->headers);
+    }
+}
