65456683308a388ab61fa5ed47da01579718baa2
Tatevik <tatevikg1@gmail.com>
2025-03-23 21:24:59 +0400

    Fix documentation title

--- src/PhpListRestBundle.php
+++ src/PhpListRestBundle.php
@@ -14,8 +14,8 @@ use OpenApi\Attributes as OA;
  */
 #[OA\Info(
     version: '1.0.0',
-    description: 'This is the OpenAPI documentation for My API.',
-    title: 'My API Documentation',
+    description: 'This is the OpenAPI documentation for PhpList API.',
+    title: 'PhpList API Documentation',
     contact: new OA\Contact(
         email: 'support@phplist.com'
     ),
