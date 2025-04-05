253d39decb3d028c9afaa5030d0d89c833d543b2
Tatevik <tatevikg1@gmail.com>
2025-04-05 11:08:01 +0400

    ISSUE-345: return 422

--- src/Validator/RequestValidator.php
+++ src/Validator/RequestValidator.php
@@ -6,9 +6,11 @@ namespace PhpList\RestBundle\Validator;
 
 use PhpList\RestBundle\Entity\RequestInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Throwable;
 
 class RequestValidator
 {
@@ -19,12 +21,19 @@ class RequestValidator
 
     public function validate(Request $request, string $dtoClass): RequestInterface
     {
-        $dto = $this->serializer->deserialize($request->getContent(), $dtoClass, 'json');
-
+        try {
+            $dto = $this->serializer->deserialize(
+                $request->getContent(),
+                $dtoClass,
+                'json'
+            );
+        } catch (Throwable $e) {
+            throw new UnprocessableEntityHttpException('Invalid JSON: ' . $e->getMessage());
+        }
         $errors = $this->validator->validate($dto);
 
         if (count($errors) > 0) {
-            throw new BadRequestHttpException((string) $errors);
+            throw new UnprocessableEntityHttpException((string) $errors);
         }
 
         return $dto;
