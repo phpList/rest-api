0d367efed133ba97f7e88ca370aa75b5e15e022c
Tatevik <tatevikg1@gmail.com>
2025-04-22 21:08:53 +0400

    ISSUE-345: template by id

--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace PhpList\RestBundle\Controller;
 
 use OpenApi\Attributes as OA;
+use PhpList\Core\Domain\Model\Messaging\Template;
 use PhpList\Core\Domain\Repository\Messaging\TemplateRepository;
 use PhpList\Core\Security\Authentication;
 use PhpList\RestBundle\Controller\Traits\AuthenticationTrait;
 use PhpList\RestBundle\Serializer\TemplateNormalizer;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -81,5 +83,51 @@ class TemplateController extends AbstractController
         }, $data);
 
         return new JsonResponse($normalized, Response::HTTP_OK);
+    }
+
+    #[Route('/{templateId}', name: 'get_template', methods: ['GET'])]
+    #[OA\Get(
+        path: '/templates/{templateId}',
+        description: 'Returns template by id.',
+        summary: 'Gets a templateI by id.',
+        tags: ['templates'],
+        parameters: [
+            new OA\Parameter(
+                name: 'session',
+                description: 'Session ID obtained from authentication',
+                in: 'header',
+                required: true,
+                schema: new OA\Schema(
+                    type: 'string'
+                )
+            ),
+            new OA\Parameter(
+                name: 'templateId',
+                description: 'template ID',
+                in: 'path',
+                required: true,
+                schema: new OA\Schema(type: 'string')
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Success',
+                content: new OA\JsonContent(ref: '#/components/schemas/Template')
+            ),
+            new OA\Response(
+                response: 403,
+                description: 'Failure',
+                content: new OA\JsonContent(ref: '#/components/schemas/UnauthorizedResponse')
+            )
+        ]
+    )]
+    public function getTemplate(
+        Request $request,
+        #[MapEntity(mapping: ['templateId' => 'id'])] Template $template
+    ): JsonResponse {
+        $this->requireAuthentication($request);
+
+        return new JsonResponse($this->normalizer->normalize($template), Response::HTTP_OK);
     }
 }
