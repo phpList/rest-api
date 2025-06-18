d524ab515e3f78d5c90ac46fe52d490c786ffd4e
Tatevik <tatevikg1@gmail.com>
2025-06-18 23:30:07 +0400

    ISSUE-345: AccessDeniedException

--- src/Common/EventListener/ExceptionListener.php
+++ src/Common/EventListener/ExceptionListener.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Validator\Exception\ValidatorException;
 
 class ExceptionListener
@@ -45,6 +46,11 @@ class ExceptionListener
             $response = new JsonResponse([
                 'message' => $exception->getMessage(),
             ], 400);
+            $event->setResponse($response);
+        } elseif ($exception instanceof AccessDeniedException) {
+            $response = new JsonResponse([
+                'message' => $exception->getMessage(),
+            ], 403);
             $event->setResponse($response);
         } elseif ($exception instanceof Exception) {
             $response = new JsonResponse([
