4467266e5d708c603f78b9d2bd4db52cd8a83c82
Tatevik <tatevikg1@gmail.com>
2026-04-08 15:54:44 +0400

    Add: validateNoClickTrackLinks

--- src/Messaging/Request/Message/MessageContentRequest.php
+++ src/Messaging/Request/Message/MessageContentRequest.php
@@ -7,6 +7,7 @@ namespace PhpList\RestBundle\Messaging\Request\Message;
 use OpenApi\Attributes as OA;
 use PhpList\Core\Domain\Messaging\Model\Dto\Message\MessageContentDto;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 #[OA\Schema(
     schema: 'MessageContentRequest',
@@ -20,6 +21,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 )]
 class MessageContentRequest implements RequestDtoInterface
 {
+    private const CLICKTRACK_MESSAGE = 'You should not paste the results of a test message back into the editor. '
+    . 'This will break the click-track statistics, and overload the server.';
+
     #[Assert\NotBlank]
     public string $subject;
 
@@ -28,6 +32,25 @@ class MessageContentRequest implements RequestDtoInterface
 
     #[Assert\NotBlank]
     public string $footer;
+
+    #[Assert\Callback('validateNoClickTrackLinks')]
+    public function validateNoClickTrackLinks(ExecutionContextInterface $context): void
+    {
+        if (!isset($this->text)) {
+            return;
+        }
+
+        $hasClickTrackLinks = preg_match('/lt\.php\?id=[\w%]{22}/', $this->text) === 1
+            || preg_match('/lt\.php\?id=[\w%]{16}/', $this->text) === 1
+            || preg_match('#/lt/[\w%]{22}#', $this->text) === 1
+            || preg_match('#/lt/[\w%]{16}#', $this->text) === 1;
+
+        if ($hasClickTrackLinks) {
+            $context->buildViolation(self::CLICKTRACK_MESSAGE)
+                ->atPath('text')
+                ->addViolation();
+        }
+    }
 
     public function getDto(): MessageContentDto
     {
