8e2df6596f37a1f663963ac60f16d02ee6c104ba
Tatevik <tatevikg1@gmail.com>
2026-09-24 16:51:16 +0400

    Update SubscribePagePublicNormalizer options serialization

--- src/Subscription/Serializer/SubscribePagePublicNormalizer.php
+++ src/Subscription/Serializer/SubscribePagePublicNormalizer.php
@@ -41,7 +41,14 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
                             new OA\Property(
                                 property: 'options',
                                 type: 'array',
-                                items: new OA\Items(type: 'object')
+                                items: new OA\Items(
+                                    properties: [
+                                        new OA\Property(property: 'id', type: 'integer'),
+                                        new OA\Property(property: 'name', type: 'string'),
+                                        new OA\Property(property: 'list_order', type: 'integer'),
+                                    ],
+                                    type: 'object'
+                                )
                             ),
                         ],
                         type: 'object'
@@ -128,7 +135,14 @@ class SubscribePagePublicNormalizer implements NormalizerInterface
                 'required' => $attributeDefinition->isRequired(),
                 'default_value' => $attributeDefinition->getDefaultValue(),
                 'list_order' => $attributeDefinition->getListOrder(),
-                'options' => $attributeDefinition->getOptions(),
+                'options' => array_map(
+                    static fn ($option) => [
+                        'id' => $option->id,
+                        'name' => $option->name,
+                        'list_order' => $option->listOrder,
+                    ],
+                    $attributeDefinition->getOptions()
+                ),
             ];
         }
 
